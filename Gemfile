b0ff4243a72b0522bb802ced84a0c0761a4054f2
Tejas Manohar <me@tejas.io>
2015-09-05 04:35:31 -0500

    execute code on the fly

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'http',               '0.9.4'
+gem 'sinatra',            '1.4.6'
---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'http'
+require 'json'
+require 'sinatra'
+
+helpers do
+  def eval_in(code, language)
+    result = HTTP.with_headers(:'User-Agent' => 'tejasmanohar/pasterunner')
+     .post('https://eval.in/', :form => {
+       :utf8 => 'λ',
+       :code => code,
+       :execute => 'on',
+       :lang => language
+     })
+    location = result['location']
+    data = JSON.parse HTTP.get(location + '.json')
+    data['output']
+  end
+end
+
+post '/exec' do
+  output = eval_in code, params[:language]
+  { :stdout => output }
+end
